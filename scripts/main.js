547f469d1b4d336dc45913b8c670b449ce83cb16
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-11-16 16:46:30 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,49 @@
 // Clicking in the box toggles fullscreen on and off.
 function setup() {
   createCanvas(1200, 500);
-  background(200);
+  background(200, 24, 144);
   noLoop();
 }
 function draw() {
+  
   strokeWeight(3); 
-  line(20, 140, 70, 440);
-  rectMode(CORNER);
-  ellipseMode(CENTER);
-  strokeCap(ROUND);
+ 
+
+  fill(255, 0, 0);
+  stroke(0, 0, 255)
   rect(0, 0, 100, 100, 10);
   rectMode(CENTER);
-  rect(100, 100, 30, 40, 40);
-  ellipse(330, 430, 330, 70)
+  let x = 240;
+  let y = 440;
+  rect(x, y, 30, 40, 40);
+  ellipseMode(CENTER);
+
+
+  erase(40, 80);
+  ellipse(330, 430, 330, 70);
   circle(260, 40, 23, 4);
+  noStroke();
   triangle(750, 200, 900, 300, 555, 222);
+  noErase();
+}
+  
+/*
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(45, 45, 45);
+  strokeWeight(4);
+  stroke(160);
+  rect(15, 10, 55, 55);
+  noFill();
+  rect(20, 20, 60, 60);
+  triangle(150, 100, 400, 200, 155, 122);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(45, 45, 45);
 }
 
 
-
+/*
 function mousePressed() {
   if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
     let fs = fullscreen();
@@ -34,4 +58,4 @@ let words = ['apple', 'bear', 'cat', 'dog'];
 let word = random(words);
 text(word, 10, 50);
 line(50, r, x, y);
-}
+}*/
