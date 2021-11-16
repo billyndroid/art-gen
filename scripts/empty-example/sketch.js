0c932f3fd7f3278212400f49014e556eb02d7e1c
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-11-16 12:50:59 +0000

    Update sketch.js

--- scripts/empty-example/sketch.js
+++ scripts/empty-example/sketch.js
@@ -1,12 +1,14 @@
+let detailX;
+// slide to see how detailX works
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1350, 500, WEBGL);
+  detailX = createSlider(3, 24, 3);
+  detailX.position(10, height + 5);
+  detailX.style('width', '80px');
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(205, 105, 94);
+  rotateY(millis() / 1000);
+  sphere(40, detailX.value(), 16);
 }
