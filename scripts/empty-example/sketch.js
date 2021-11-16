e19647caf4804f612358a900403f6dd5064e4b9d
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-11-16 15:10:14 +0000

    update

--- scripts/empty-example/sketch.js
+++ scripts/empty-example/sketch.js
@@ -1,12 +1,32 @@
-// Clicking in the box toggles fullscreen on and off.
+let rectX = 0;
+let fr = 30; //starting FPS
+let clr;
+
 function setup() {
   background(200);
+  frameRate(fr); // Attempt to refresh at starting FPS
+  clr = color(255, 0, 0);
 }
-function mousePressed() {
-  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-    let fs = fullscreen();
-    fullscreen(!fs);
-    background(205, 102, 94);
-  sphere(40);
+
+function draw() {
+  background(200);
+  rectX = rectX += 5; // Move Rectangle
+
+  if (rectX >= width) {
+   // If you go off screen.
+    if (fr === 30) {
+      clr = color(0, 0, 255);
+      fr = 10;
+      frameRate(fr); // make frameRate 10 FPS
+    } else {
+      clr = color(255, 0, 0);
+      fr = 30;
+      frameRate(fr); // make frameRate 30 FPS
+    }
+    rectX = 0;
   }
+  fill(clr);
+  rect(rectX, 40, 20, 20);
 }
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,29 @@
 // Clicking in the box toggles fullscreen on and off.
 function setup() {
-  createCanvas(1200, 100);
-    background(200);
+  createCanvas(1200, 500);
+  background(200);
+  noLoop();
+}
+function draw() {
+  strokeWeight(6); // Thicker
+ line(20, 40, 80, 40);
+ ellipse(20, 50, 50, 20);
+}
+
+
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+    
   }
-  function mousePressed() {
-    if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-      let fs = fullscreen();
-      fullscreen(!fs);
-      
-    }
-  }
- function draw() {
-     strokeWeight(6); // Thicker
-    line(20, 40, 80, 40);
-    ellipse(20, 50, 50, 20);
-    sphere(40);
- }
+}
+for (let y = 0; y < 100; y++) {
+let x = randomGaussian(10, 15);
+let r = random(-50, 50);
+let words = ['apple', 'bear', 'cat', 'dog'];
+let word = random(words);
+text(word, 10, 50);
+line(50, r, x, y);
+}
