5463c92c220f7d002eabc50c1123c762110e23ba
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-11-20 07:06:28 +0000

    Update art3.js

--- scripts/art3.js
+++ scripts/art3.js
@@ -1,14 +1,35 @@
-let color = 255;
+let x = 200;
+let y = 200;
+let canvasLayer2;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(color);
-  }
-function draw() {
-    noFill();
-    stroke(10, 0, 0);
-    bezier(185, 20, 10, 10, 90, 90, 15, 80);
-
+  createCanvas(windowWidth, windowHeight);
+  canvasLayer2 = createGraphics(windowWidth, windowHeight);
+  canvasLayer2.clear();
+  background(0);
 }
 
+function draw() {
+  
+  // no trails
+  background(0);
+  x += random(-5, 15);
+  y += random(-5, 5);
+	
+  // trails
 
+    canvasLayer2.fill(255, 150);
+    canvasLayer2.noStroke();
+    let starX = random(width);
+    let starY = random(height);
+    canvasLayer2.ellipse(starX, starY, 10, 10);
+   
+  
+  image(canvasLayer2, 0, 0);
+  fill(mouseX, y, 255);
+  stroke(10);
+  rectMode(CENTER);
+  rect(x, y, x, y);
+
+  
+}
